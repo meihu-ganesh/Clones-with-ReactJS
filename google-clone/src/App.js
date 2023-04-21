4b6415cab65b57299d4463fed72f8cbd608cfb74
CSNR Ganesh <100460284+meihu-ganesh@users.noreply.github.com>
2023-04-21 16:51:58 +0530

    created basic structure

---
+++ google-clone/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Home from './Pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets build Google Clone 🚀 !  </h1>
+
+      {/* Home Page (The one with search on) */}
+      <Home />
+
+      {/* search Page (Results Page) */}
+    </div>
+  );
+}
+
+export default App;
---
+++ google-clone/src/Pages/Home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <h1>This is home page</h1>
+
+      <div className="home__header">
+        <div className="home__headerLeft">
+          {/* Link */}
+          {/* Link */}
+        </div>
+        <div className="home__headerRight">
+          {/* Link */}
+          {/* Link */}
+          {/* Icon */}
+          {/* Avatar */}
+
+        </div>
+      </div>
+      <div className="home__body">
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
